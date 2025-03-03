4336eb49d92573d0af5d86a843ce31bb048721fb
Gaurawsfdc <gaurawbhute012@gmail.com>
2025-03-03 22:48:34 +0530

    First commit

--- src/controlstatement/SwitchCalculator.java
+++ src/controlstatement/SwitchCalculator.java
@@ -1,4 +1,20 @@
 package controlstatement;
 
+import java.util.Scanner;
+
 public class SwitchCalculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the num1: ");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Enter the num2: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Enter the operator: ");
+        char operator = sc.next().charAt(0);
+
+
+    }
 }
