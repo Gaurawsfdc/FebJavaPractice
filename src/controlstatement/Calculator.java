228611c51b02b7568766a37f2635973db2447fb7
Gaurawsfdc <gaurawbhute012@gmail.com>
2025-03-04 06:21:35 +0530

    second commit

--- src/controlstatement/Calculator.java
+++ src/controlstatement/Calculator.java
@@ -1,4 +1,23 @@
 package controlstatement;
 
 public class Calculator {
+    public int num1 =10;
+    public int num2 = 30;
+
+    public void addNum(int num1){
+        this.num1 = num1;
+        int add = num1 + num2;
+        System.out.println("result of addition = " + add);
+    }
+    public void substract(){
+        System.out.println("Substraction = " + (num1-num2));
+    }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.num1 = 25;
+        c.addNum(c.num1);
+        c.substract();
+
+    }
 }
