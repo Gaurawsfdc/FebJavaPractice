c897a61280eb30b7de0a9bfc8cf563fefc5bb640
Gaurawsfdc <gaurawbhute012@gmail.com>
2025-04-10 06:30:59 +0530

    10 april

---
+++ src/collectionframework/HashTableExamples.java
@@ -0,0 +1,9 @@
+package collectionframework;
+
+import java.util.Hashtable;
+
+public class HashTableExamples {
+    public static void main(String[] args) {
+        Hashtable<Integer, String>
+    }
+}
